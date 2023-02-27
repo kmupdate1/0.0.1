a6455c46fda2ad067d37160ae0c46e7ac4923013
kmupdate1 <lax.potcx1562@gmail.com>
2023-02-28 01:14:09 +0900

    IDを含むパス検索がまだできてない

--- src/main/kotlin/jp/wataju/plugins/Templating.kt
+++ src/main/kotlin/jp/wataju/plugins/Templating.kt
@@ -156,162 +156,192 @@ fun Application.configureTemplating() {
         get("/search-condition") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify
-            )
+            if ( session.accountId != null ) {
+                val model = mapOf(
+                    "tab_title" to TAB_TITLE,
+                    "title" to TITLE,
+                    "identify" to session.identify
+                )
 
-            call.respond(MustacheContent("top/top.hbs", model))
+                call.respond(MustacheContent("top/top.hbs", model))
+            } else {
+                call.respondRedirect("/login")
+            }
         }
 
         // お客様情報
         get("/customer-list") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            val customers = mutableListOf<jp.wataju.model.entity.Customer>()
-            connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
-            transaction {
-                Customer.selectAll().limit(10)
-                    .forEach {
-                        customers.add(jp.wataju.model.entity.Customer(it))
-                    }
+            if ( session.accountId != null ) {
+                val customers = mutableListOf<jp.wataju.model.entity.Customer>()
+                connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
+                transaction {
+                    Customer.selectAll().limit(10)
+                        .forEach {
+                            customers.add(jp.wataju.model.entity.Customer(it))
+                        }
+                }
+
+                val model = mapOf(
+                    "tab_title" to TAB_TITLE,
+                    "title" to TITLE,
+                    "identify" to session.identify,
+                    "page_message" to CUSTOMER_INFORMATION_LIST,
+                    "customers" to customers,
+                    "customer_detail" to false
+                )
+
+                call.respond(MustacheContent("customer/customer.hbs", model))
+            } else {
+                call.respondRedirect("/login")
             }
-
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify,
-                "page_message" to CUSTOMER_INFORMATION_LIST,
-                "customers" to customers,
-                "customer_detail" to false
-            )
-
-            call.respond(MustacheContent("customer/customer.hbs", model))
         }
         get("/customer-detail-7cee08fe-5fa0-456b-b969-77aae6f4481d") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            var customer: jp.wataju.model.entity.Customer? = null
-            connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
-            transaction {
-                val query = Customer.select { Customer.customerId eq UUID.fromString("7cee08fe-5fa0-456b-b969-77aae6f4481d")}
-                kotlin.runCatching {
-                    customer = jp.wataju.model.entity.Customer(query.first())
+            if ( session.accountId != null ) {
+                var customer: jp.wataju.model.entity.Customer? = null
+                connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
+                transaction {
+                    val query =
+                        Customer.select { Customer.customerId eq UUID.fromString("7cee08fe-5fa0-456b-b969-77aae6f4481d") }
+                    kotlin.runCatching {
+                        customer = jp.wataju.model.entity.Customer(query.first())
+                    }
                 }
+
+                val model = mapOf(
+                    "tab_title" to TAB_TITLE,
+                    "title" to TITLE,
+                    "identify" to session.identify,
+                    "page_message" to CUSTOMER_INFORMATION_DETAIL,
+                    "customer" to customer,
+                    "customer_detail" to true
+                )
+
+                call.respond(MustacheContent("customer/customer.hbs", model))
+            } else {
+                call.respondRedirect("/login")
             }
-
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify,
-                "page_message" to CUSTOMER_INFORMATION_DETAIL,
-                "customer" to customer,
-                "customer_detail" to true
-            )
-
-            call.respond(MustacheContent("customer/customer.hbs", model))
         }
 
         // 商品情報
         get("/product-list") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            val products = mutableListOf<jp.wataju.model.entity.Product>()
-            connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
-            transaction {
-                Product.selectAll().limit(10)
-                    .forEach {
-                        products.add(jp.wataju.model.entity.Product(it))
-                    }
+            if ( session.accountId != null ) {
+                val products = mutableListOf<jp.wataju.model.entity.Product>()
+                connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
+                transaction {
+                    Product.selectAll().limit(10)
+                        .forEach {
+                            products.add(jp.wataju.model.entity.Product(it))
+                        }
+                }
+
+                val model = mapOf(
+                    "tab_title" to TAB_TITLE,
+                    "title" to TITLE,
+                    "identify" to session.identify,
+                    "page_message" to PRODUCT_INFORMATION_LIST,
+                    "products" to products,
+                    "product_detail" to false
+                )
+
+                call.respond(MustacheContent("product/product.hbs", model))
+            } else {
+                call.respondRedirect("/login")
             }
-
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify,
-                "page_message" to PRODUCT_INFORMATION_LIST,
-                "products" to products,
-                "product_detail" to false
-            )
-
-            call.respond(MustacheContent("product/product.hbs", model))
         }
         get("/product-detail-6d1a4380-83d9-404b-9b14-6557a506f3fc") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            var product: jp.wataju.model.entity.Product? = null
-            connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
-            transaction {
-                val query = Product.select { Product.productId eq UUID.fromString("6d1a4380-83d9-404b-9b14-6557a506f3fc") }
-                kotlin.runCatching {
-                    product = jp.wataju.model.entity.Product(query.first())
+            if ( session.accountId != null ) {
+                var product: jp.wataju.model.entity.Product? = null
+                connect(PLATFORM_WINDOWS, CUSTOMER_MANAGEMENT_SYSTEM)
+                transaction {
+                    val query =
+                        Product.select { Product.productId eq UUID.fromString("6d1a4380-83d9-404b-9b14-6557a506f3fc") }
+                    kotlin.runCatching {
+                        product = jp.wataju.model.entity.Product(query.first())
+                    }
                 }
+
+                val model = mapOf(
+                    "tab_title" to TAB_TITLE,
+                    "title" to TITLE,
+                    "identify" to session.identify,
+                    "page_message" to PRODUCT_INFORMATION_DETAIL,
+                    "product" to product,
+                    "product_detail" to true
+                )
+
+                call.respond(MustacheContent("product/product.hbs", model))
+            } else {
+                call.respondRedirect("/login")
             }
-
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify,
-                "page_message" to PRODUCT_INFORMATION_DETAIL,
-                "product" to product,
-                "product_detail" to true
-            )
-
-            call.respond(MustacheContent("product/product.hbs", model))
         }
 
         // ユーザ設定
         get("/setting") {
             val session = call.sessions.get() ?: UserSession(null, null)
 
-            val model = mapOf(
-                "tab_title" to TAB_TITLE,
-                "title" to TITLE,
-                "identify" to session.identify,
-                "flag_confirm" to false
-            )
-
-            call.respond(MustacheContent("setting/setting.hbs", model))
-        }
-        post("/registry-confirm") {
-            val session = call.sessions.get() ?: UserSession(null, null)
-
-            val userSettingParams = call.receiveParameters()
-            val userName = userSettingParams["user-name"] ?: ""
-            val userNameKana = userSettingParams["user-name-kana"] ?: ""
-            val userPhone = userSettingParams["user-phone"] ?: ""
-            val userMail = userSettingParams["user-mail"] ?: ""
-
-            if (userName != "") {
-
-                val data = arrayOf(
-                    mapOf("label" to "ユーザ名", "element" to userName),
-                    mapOf("label" to "ユーザ名（カナ）", "element" to userNameKana),
-                    mapOf("label" to "電話番号", "element" to userPhone),
-                    mapOf("label" to "メールアドレス", "element" to userMail)
-                )
-
+            if ( session.accountId != null ) {
                 val model = mapOf(
                     "tab_title" to TAB_TITLE,
                     "title" to TITLE,
                     "identify" to session.identify,
-                    "user_data" to data,
-                    "flag_confirm" to true,
-                )
-
-                call.respond(MustacheContent("setting/setting.hbs", model))
-            } else {
-                val model = mapOf(
-                    "tab_title" to TAB_TITLE,
-                    "title" to TITLE,
-                    "identify" to session.identify,
-                    "message" to UNSUCCESSFUL_REGISTRY_USER,
-                    "message_alert" to false,
                     "flag_confirm" to false
                 )
 
                 call.respond(MustacheContent("setting/setting.hbs", model))
+            } else {
+                call.respondRedirect("/login")
+            }
+        }
+        post("/registry-confirm") {
+            val session = call.sessions.get() ?: UserSession(null, null)
+
+            if ( session.accountId != null ) {
+                val userSettingParams = call.receiveParameters()
+                val userName = userSettingParams["user-name"] ?: ""
+                val userNameKana = userSettingParams["user-name-kana"] ?: ""
+                val userPhone = userSettingParams["user-phone"] ?: ""
+                val userMail = userSettingParams["user-mail"] ?: ""
+
+                if (userName != "") {
+
+                    val data = arrayOf(
+                        mapOf("label" to "ユーザ名", "element" to userName),
+                        mapOf("label" to "ユーザ名（カナ）", "element" to userNameKana),
+                        mapOf("label" to "電話番号", "element" to userPhone),
+                        mapOf("label" to "メールアドレス", "element" to userMail)
+                    )
+
+                    val model = mapOf(
+                        "tab_title" to TAB_TITLE,
+                        "title" to TITLE,
+                        "identify" to session.identify,
+                        "user_data" to data,
+                        "flag_confirm" to true,
+                    )
+
+                    call.respond(MustacheContent("setting/setting.hbs", model))
+                } else {
+                    val model = mapOf(
+                        "tab_title" to TAB_TITLE,
+                        "title" to TITLE,
+                        "identify" to session.identify,
+                        "message" to UNSUCCESSFUL_REGISTRY_USER,
+                        "message_alert" to false,
+                        "flag_confirm" to false
+                    )
+
+                    call.respond(MustacheContent("setting/setting.hbs", model))
+                }
+            } else {
+                call.respondRedirect("/login")
             }
 
         }
